02982dc27fb77f0418984b3f0b511e3361e54fa3
WaheedAhmed74 <159169508+WaheedAhmed74@users.noreply.github.com>
2024-03-04 18:06:21 +0500

    https://github.com/WaheedAhmed74/Challenge_D3_208505.git
    Challenge Day 1, Day 2 and Day 3

---
+++ D1Q1.js
@@ -0,0 +1,35 @@
+// // Challenge : Day 1 (Question 1)
+// Install  Node.Js then Typescript and then VS Code
+// // Challenge : Day 1 (Question 2)
+// create a name in a variable
+// let name1: string = "Waheed"
+// console.log("Hellow Mr. "+name1+ " Would You like to learn some Typescript Today")
+// console.log("Hellow Mr."+ " " + name1 + " " + "Would You like to learn some Typescript Today")
+// // Challenge : Day 1 (Question 3)
+// create a name in a variable
+// let myname : string = "wAheEd aHmEd sOomRO"
+// console.log(" My Name in Orignal Text " + myname)
+// console.log(" My Name in LowerCase Text " + myname.toLowerCase())
+// console.log(" My Name in UpperCase Text " + myname.toUpperCase())
+// this Line learn from Internet but still no idea how it works (Title Case)
+// console.log("My Nam in TitleCase Text " + myname.toLowerCase().replace(/(?:^|\s)\w/g, match => match.toUpperCase()))
+// // Challenge : Day 2 (Question 1)
+// console.log(`"Albert Einstein once said, "A person who never made a mistake never tried anything new.""`)
+// // Challenge : Day 2 (Question 2)
+// let famous_person : string = "Albert Einstein";
+// let message : string = "A person who never made a mistake never tried anything new.";
+// console.log(`"${famous_person} once said, "${message}""`)
+// // Challenge : Day 2 (Question 3)
+// let stripping_name : string = "     Waheed   \t    \n"
+// console.log (stripping_name)
+// console.log (stripping_name.trim())
+// // Challenge : Day 3 (Question 1)
+// console.log(5+3)
+// console.log(9-1)
+// console.log(4*2)
+// console.log(16/2)
+// // Challenge : Day 3 (Question 2)
+console.log("5+3\n7+1\n4+4\n9-1");
+// // Challenge : Day 3 (Question 3)
+// var favourite_number : number = 7;
+// console.log("Here is my favourite number " + favourite_number)
